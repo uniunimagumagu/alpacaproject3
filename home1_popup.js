6113bc9a00b951b2004a799c7e9caaf77cdc949d
uniunimagumagu <truepeace023@gmail.com>
2023-05-10 12:40:40 +0900

    CSSの作成

--- home1_popup.js
+++ home1_popup.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 "use strict";
 const popupImg = document.querySelector('.popup-img');
 setTimeout(() => {
   popupImg.style.bottom = '50px';
   popupImg.style.opacity = '1';
-=======
-"use strict";
-const popupImg = document.querySelector('.popup-img');
-setTimeout(() => {
-  popupImg.style.bottom = '50px';
-  popupImg.style.opacity = '1';
->>>>>>> 89f5758b5312e426a9b16c909cbf242f8aa919f9
 }, 2000);
