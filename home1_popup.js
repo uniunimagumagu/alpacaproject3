1c33dfd78fe9060c8903b8c5e1ffd041f73d6b9d
uniunimagumagu <truepeace023@gmail.com>
2023-05-12 13:57:48 +0900

    修正

--- home1_popup.js
+++ home1_popup.js
@@ -3,5 +3,5 @@
 const popupImg = document.querySelector('.popup-img');
 setTimeout(() => {
   popupImg.style.bottom = '50px';
-  popupImg.style.opacity = '1';
+    popupImg.style.opacity = '1';
 }, 2000);
