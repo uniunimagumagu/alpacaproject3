de162f00eb41badbe90bfb1e671946947427e134
uniunimagumagu <truepeace023@gmail.com>
2023-05-19 00:28:18 +0900

    修正5/19

--- home1_popup.js
+++ home1_popup.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//当初は適当な位置に浮かび上がってくるだけのものだったがページサイズを変更すると表示がバグるので常にページの表示サイズを監視することで再計算させて表示を正しくした。
 const popupImg = document.querySelector('.popup-img');
 let bottomPosition = 0; // 初期の表示位置
 
