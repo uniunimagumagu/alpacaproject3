7c02d3a9179c98c2ee62cb192f875d6185d1e928
Fuezako <n.0901onagaoka@gmail.com>
2023-11-13 03:56:54 +0900

    ポップアップ領域を画像サイズに合わせて動的に変化

--- home1_popup.js
+++ home1_popup.js
@@ -3,12 +3,16 @@
 const popupImg = document.querySelector('.popup-img');
 let bottomPosition = 0; // 初期の表示位置
 
+const popupArea = document.getElementById('popup-area');/*画面サイズ変更の度に更新されるcalc-pu-posに便乗してheightを変化*/
+
 function calculatePopupPosition() {
   const windowHeight = window.innerHeight; // ウィンドウの高さを取得
   const popupHeight = popupImg.offsetHeight; // ポップアップの高さを取得
 
   bottomPosition = -popupHeight +windowHeight; // ポップアップの下端がウィンドウの下端に合う位置を計算。これがすごい。
   popupImg.style.bottom = `${bottomPosition-50}px`; // ポップアップの位置を設定.いまはタイトルにかぶらないように5０だけどこの数字で調整できる。
+
+  popupArea.style.height = `${popupHeight}px`;// 画像に隠される領域の高さを更新
 }
 
 function handleWindowResize() {
